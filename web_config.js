231eda8ed3cdcc4e730a878f4695c4d6ad2fb5d5
acquired <acquired.way@gmail.com>
2025-08-09 05:59:35 +0700

    Configure Supabase URL and anon key for cloud sync

--- web_config.js
+++ web_config.js
@@ -1,8 +1,8 @@
 // Опциональная конфигурация Supabase для облачной базы.
 // Оставьте пустым, чтобы Mini App работал только на localStorage.
 window.WEB_CONFIG = {
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: '',
+  SUPABASE_URL: 'https://uhhsrtmmuwoxsdquimaa.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVoaHNydG1tdXdveHNkcXVpbWFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2OTMwMzcsImV4cCI6MjA3MDI2OTAzN30.5xxo6g-GEYh4ufTibaAtbgrifPIU_ilzGzolAdmAnm8',
 };
 
 
